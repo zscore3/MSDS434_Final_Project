6d1b7bde6bdd36f53a8778159e25fc5c3b2ec495
zscore3 <60632046+zscore3@users.noreply.github.com>
2025-04-14 20:28:29 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import(
   "time"
   )
 
-func main()(
+func main(){
   http.HandleFunc("/", index)
   fmt.Println("Now Serving localhost:8000")
   http.ListenAndServe(":8000",nil)
-)
+}
 
-func index(w http.ResponseWriter, r *http.Request)(
-  fmt.FPrintf(w, "Hello, this is a web page! %s", time.Now())
-)
+func index(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Hello, this is a web page! %s", time.Now())
+}
 

3d5cc895b175808eb4907fc2886a56015af234e7
zscore3 <60632046+zscore3@users.noreply.github.com>
2025-04-14 20:26:14 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import( 
+  "fmt"
+  "net/http"
+  "time"
+  )
+
+func main()(
+  http.HandleFunc("/", index)
+  fmt.Println("Now Serving localhost:8000")
+  http.ListenAndServe(":8000",nil)
+)
+
+func index(w http.ResponseWriter, r *http.Request)(
+  fmt.FPrintf(w, "Hello, this is a web page! %s", time.Now())
+)
+
